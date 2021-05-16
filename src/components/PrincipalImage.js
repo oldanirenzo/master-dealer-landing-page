201041b6738c06f6d1894609e11f493fe28c80c3
Renzo Oldani <oldanirenzo@gmail.com>
2021-05-16 19:33:27 -0300

    Changing the principal image

--- src/components/PrincipalImage.js
+++ src/components/PrincipalImage.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import roof from '../assets/images/roof.svg'
+// import roof from '../assets/images/roof.svg'
+import principalImage from '../assets/images/principal-image.svg'
 import '../assets/css/image.css'
 
 export const PrincipalImage = () => {
     return (
         <div className="principal-image">
-            <img src={roof} alt='Roof' />
+            <img src={principalImage} alt='Roof' />
         </div>
     )
 }
